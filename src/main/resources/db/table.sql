291181362b192659646c7608c13ab56fbd1a2ae4
Kjuk <juk6340@gmail.com>
2023-01-26 11:37:21 +0900

    더미데이터 생성 완료

---
+++ src/main/resources/db/table.sql
@@ -0,0 +1,26 @@
+insert into user_tb(username, password, email, created_at) values('ssar', '1234', 'ssar@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('cos', '1234', 'cos@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('guk', '1234', 'guk@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('kim', '1234', 'kim@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('jeong', '1234', 'jeong@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('uk', '1234', 'uk@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('ji', '1234', 'ji@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('ya', '1234', 'ya@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('nol', '1234', 'nol@nate.com', now());
+insert into user_tb(username, password, email, created_at) values('ak', '1234', 'ak@nate.com', now());
+
+insert into board_tb(title, userId, created_at) values('첫번째 글입니다.', 1, now());
+insert into board_tb(title, userId, created_at) values('두번째 글입니다.', 2, now());
+insert into board_tb(title, userId, created_at) values('세번째 글입니다.', 3, now());
+insert into board_tb(title, userId, created_at) values('네번째 글입니다.', 4, now());
+insert into board_tb(title, userId, created_at) values('다섯번째 글입니다.', 5, now());
+insert into board_tb(title, userId, created_at) values('여섯번째 글입니다.', 6, now());
+insert into board_tb(title, userId, created_at) values('일곱번째 글입니다.', 7, now());
+insert into board_tb(title, userId, created_at) values('여덟번째 글입니다.', 8, now());
+insert into board_tb(title, userId, created_at) values('아홉번째 글입니다.', 9, now());
+insert into board_tb(title, userId, created_at) values('열번째 글입니다.', 10, now());
+
+
+
+
+commit;
